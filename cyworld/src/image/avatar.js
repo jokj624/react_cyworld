4df2b84aa5fad3457b15082c69795ad8c4bd4ec6
jokj624 <jokj624@gmail.com>
2022-06-20 19:25:07 +0900

    Feat: font, image

---
+++ cyworld/src/image/avatar.js
@@ -0,0 +1,9 @@
+module.exports = [
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar1.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar2.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar3.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar4.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar5.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar6.png",
+    "https://sopt-connector.s3.ap-northeast-2.amazonaws.com/avatar7.png"
+];
---
+++ cyworld/src/image/galleryImage.js
@@ -0,0 +1,176 @@
+module.exports = [
+    {
+        title: "[스크랩] 밀회",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%AD%E1%84%89%E1%85%B5%E1%86%A8%E1%84%8B%E1%85%B5%E1%84%85%E1%85%A1%E1%86%BC%E1%84%82%E1%85%A1.jpeg"
+    }, 
+    {
+        title: "[스크랩] 잘하자 정아야",
+        writer: "이현우",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%A7%E1%86%AB%E1%84%8B%E1%85%AE%E1%84%8B%E1%85%B4%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4.jpeg"
+    },
+    {
+        title: "[스크랩] 현우와 아이들",
+        writer: "이현우",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%A7%E1%86%AB%E1%84%8B%E1%85%AE%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3%E1%86%AF.jpeg"
+    },
+    {
+        title: "[스크랩] 한강에서 ♥︎",
+        writer: "나빼고 언제감?",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%A1%E1%86%AB%E1%84%80%E1%85%A1%E1%86%BC%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "[스크랩] 학연 지연 혈연",
+        writer: "장서현",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%92%E1%85%A1%E1%86%A8%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%8C%E1%85%B5%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%92%E1%85%A7%E1%86%AF%E1%84%8B%E1%85%A7%E1%86%AB.jpeg"
+    },
+    {
+        title: "[스크랩] 절교 하자고",
+        writer: "강민재",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%8C%E1%85%A5%E1%86%AF%E1%84%80%E1%85%AD.jpeg"
+    },
+    {
+        title: "[스크랩] 한 여름, 엠티에서",
+        writer: "난 셈나 준비중이었는데",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%8B%E1%85%A6%E1%86%B7%E1%84%90%E1%85%B5%E1%84%89%E1%85%A5%E1%84%87%E1%85%A52.jpeg"
+    },
+    {
+        title: "[스크랩] 한 여름, 엠티에서 2",
+        writer: "재밌었냐?",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%8B%E1%85%A6%E1%86%B7%E1%84%90%E1%85%B5%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "솝커톤에 러시모어 뜸",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%A9%E1%86%B8%E1%84%8F%E1%85%A5%E1%84%90%E1%85%A9%E1%86%AB%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "[스크랩] 솝트 서버 최강 귀요미",
+        writer: "장서현",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%A5%E1%84%92%E1%85%A7%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%8B%E1%85%A1.jpeg"
+    },
+    {
+        title: "[스크랩] 뒷풀이에서♥︎",
+        writer: "이승헌",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%83%E1%85%B1%E1%86%BA%E1%84%91%E1%85%AE%E1%86%AF%E1%84%8B%E1%85%B5%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "[스크랩] 뒷풀이에서 팟장만 빼고♥︎",
+        writer: "장서현",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%83%E1%85%B1%E1%86%BA%E1%84%91%E1%85%AE%E1%86%AF%E1%84%8B%E1%85%B5%E1%84%82%E1%85%A1%E1%84%86%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%8B%E1%85%B3%E1%86%B7.jpeg"
+    },
+    {
+        title: "[스크랩] 구걸",
+        writer: "박정훈",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%AE%E1%84%80%E1%85%A5%E1%86%AF.jpeg"
+    },
+    {
+        title: "기획경선!",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%AC%E1%86%A8%E1%84%80%E1%85%A7%E1%86%BC%E1%84%89%E1%85%A5%E1%86%AB%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "기탄 서버",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%92%E1%85%A1%E1%84%80%E1%85%A6%E1%84%8F%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%AB%E1%84%83%E1%85%A1.jpeg"
+    },
+    {
+        title: "2차행사",
+        writer: "섭섭이",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/2%E1%84%8E%E1%85%A1%E1%84%92%E1%85%A2%E1%86%BC%E1%84%89%E1%85%A1%E1%84%89%E1%85%A5%E1%84%87%E1%85%A5.jpeg"
+    },
+    {
+        title: "두근두근 프로필 사진",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/E67E0A9F-90F2-4DEB-AB45-DF22EEC4F6B0_1_105_c.jpeg"
+    },
+    {
+        title: "진짜 개놀랐던 뒷풀이",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/3FEBEC44-A3A1-40E1-A17B-1C6D3C4ED400_1_105_c.jpeg"
+    },
+    {
+        title: "떨렸던 2차 세미나",
+        writer: "채정아",
+        date: "2022.04.09",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/F21754A8-DA52-4A8A-893D-D11778D342B1_1_105_c.jpeg"
+    },
+    {
+        title: "그만 좀 .. 먹어",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/15C79CE1-A03F-4C85-A759-7C400F418DA4_1_105_c.jpeg"
+    },
+    {
+        title: "[스크랩] 정아랑 핫식스 광고 찍은 날",
+        writer: "장서현",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/64054C3E-D1B8-4A11-B770-29FDB626C0D3_1_105_c.jpeg"
+    },
+    {
+        title: "기획경선 나빼고 술마셨니?",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/BAFE7D69-6D4A-4FF5-A7C8-53F5218AB033_1_105_c.jpeg"
+    },
+    {
+        title: "[스크랩] 잘하자.. 호영아",
+        writer: "서호영",
+        date: "2022.06.245",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/55B08B09-48DC-4A31-A65D-2E62CB59341A_1_201_a.jpeg"
+    },
+    {
+        title: "이거 근데 무슨 모임임?",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/F7098BF1-22C1-47DF-98D3-17A5C9613E34_1_102_o.jpeg"
+    },
+    {
+        title: "[스크랩] 1일 1마술 ♥︎",
+        writer: "서호영",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/1D740D1F-68E4-4857-8296-6BD3D94DBAF1_1_201_a.jpeg"
+    },
+    {
+        title: "얘네랑 술 마시면 안됨",
+        writer: "채정아",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/F4FEC9F8-3738-471E-AE5F-873F5DD3E577.jpeg"
+    },
+    {
+        title: "[스크랩] 1차 세미나!!",
+        writer: "김루희",
+        date: "2022.04.02",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/D1AE88B8-07A4-409D-BADB-9CC067655917.jpeg"
+    },
+    {
+        title: "[스크랩] 1차 세미나!!",
+        writer: "섭섭이",
+        date: "2022.04.02",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/6B1208CA-D9DC-40DA-BD80-BE109D78CE09.jpeg"
+    },
+    {
+        title: "[스크랩] 고인물 모임",
+        writer: "권세훈",
+        date: "2022.06.25",
+        url: "https://jobchae.s3.ap-northeast-2.amazonaws.com/C57C7D03-3020-4BD1-909E-008133E5A7CC.jpeg"
+    }
+];
